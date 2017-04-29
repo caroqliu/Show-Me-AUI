bb2a1b8b39f405613312b41a5506eb2c73893900
Achraf Mamdouh <achraf.mamdouh@yahoo.com>
2017-04-29 17:01:26 +0100

    Adds Notifications page

---
+++ IOS/show-me-aui/show-me-aui/MainTabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabBarController.swift
+//  show-me-aui
+//
+//  Created by Achraf Mamdouh on 4/29/17.
+//  Copyright © 2017 Achraf Mamdouh. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Force the notification view controller to be loaded.
+    for controller in self.viewControllers! {
+      if controller is NotificationViewController {
+        _ = controller.view
+      }
+    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
